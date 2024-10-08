477d1098d2230f27ad37593064c4f6812796ce8e
MarjorieEster <eu.marjorieester@gmail.com>
2024-10-08 11:07:12 -0300

    Add files via upload

---
+++ LPcomparação_0810.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <math.h> 
+int main(){
+    int idadeMarjorie = 17;
+    int idadeEster = 18;
+
+    if(idadeMarjorie > idadeEster){
+        printf("Marjorie e mais velha que Ester!");
+    }
+    else printf("Ester e mais velha que Marjorie");
+}
